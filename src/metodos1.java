70288a8fec4058cb9e6806dfcacf5b1993c99d71
Ariana Toscano Villalba <4darianatoscano@gmail.com>
2026-04-20 21:06:10 +0200

    feat: agregado metodos1

---
+++ src/metodos1.java
@@ -0,0 +1,9 @@
+public class metodos1 {
+    static int sumar(int a,  int b){
+        return a + b;
+    }
+    public static void main(String[] args) {
+        int resultado = sumar(5,3);
+        System.out.println("El resultado es : " + resultado);
+    }
+}
